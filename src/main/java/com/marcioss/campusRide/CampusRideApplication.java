cbee17435d94b340cf86c90baafe4a6f774d59f3
Marcio santos <marcioadsworks@gmail.com>
2022-03-28 13:01:54 -0300

    first commit

---
+++ src/main/java/com/marcioss/campusRide/CampusRideApplication.java
@@ -0,0 +1,13 @@
+package com.marcioss.campusRide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusRideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcioss/campusRide/CampusRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcioss.campusRide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
